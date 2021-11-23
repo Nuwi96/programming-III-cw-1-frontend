dbc0144162064d4841b8415d3c9236dfc420ec5a
Nuwi96 <92671065+Nuwi96@users.noreply.github.com>
2021-11-24 00:11:16 +0530

    change the UI design

--- src/app/views/customer/customer.component.ts
+++ src/app/views/customer/customer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-customer',
@@ -7,11 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerComponent implements OnInit {
   pVbelt: number;
-  vbelts: number;
+  vbelts: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit(): void {
+    this.vbelts = [{
+      'id': '01',
+      'name': 'Kamal',
+      'district': 'Matara',
+      'aaa': '01',
+      'bbb': '01',
+    }, {
+      'id': '02',
+      'name': 'Kamal',
+      'district': 'Matara',
+      'aaa': '01',
+      'bbb': '01',
+    }, {
+      'id': '03',
+      'name': 'Kamal',
+      'district': 'Matara',
+      'aaa': '01',
+      'bbb': '01',
+    }, {
+      'id': '04',
+      'name': 'Nimal',
+      'district': 'Matara',
+      'aaa': '01',
+      'bbb': '01',
+    }];
+  }
+
+  getRecord(record: any) {
+    console.log('record', record);
+
+  }
+
+  deleteRecord(id: any) {
+    console.log('del       record', id);
+
+  }
 }
--- src/app/views/orders/orders.component.ts
+++ src/app/views/orders/orders.component.ts
@@ -7,11 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdersComponent implements OnInit {
   vbelts: any;
-  pVbelt: string | number;
+  pVbelt: 10;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.vbelts = [{
+      "id":'01',
+      "name":'Kamal',
+      "district":'Matara',
+      "aaa":'01',
+      "bbb":'01',
+    },{
+      "id":'02',
+      "name":'Kamal',
+      "district":'Matara',
+      "aaa":'01',
+      "bbb":'01',
+    },{
+      "id":'03',
+      "name":'Kamal',
+      "district":'Matara',
+      "aaa":'01',
+      "bbb":'01',
+    },{
+      "id":'04',
+      "name":'Nimal',
+      "district":'Matara',
+      "aaa":'01',
+      "bbb":'01',
+    }]
   }
+  getRecord(record: any) {
+    console.log('record',record);
 
+  }
+  deleteRecord(id: any) {
+    console.log('del       record', id);
+
+  }
 }
