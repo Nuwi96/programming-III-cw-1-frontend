44394a876ae8489b909ddbfb7b9e7fa30b53c905
Nuwi96 <92671065+Nuwi96@users.noreply.github.com>
2021-11-29 14:13:44 +0530

    set value for dashboard paddy buying and selling tiles

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Daily_limitService} from '../../services/dily-limit/daily_limit.service';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  buyingLimit: any;
+  sellingLimit: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private daily_limitService: Daily_limitService, private datePipe: DatePipe) {
   }
 
+  ngOnInit(): void {
+    this.getAllRecordsByDate();
+  }
+
+  getAllRecordsByDate() {
+    var date = this.datePipe.transform(new Date(), 'yyyy-MM-dd');
+    this.daily_limitService.getAllRecordsByDate(date)
+      .subscribe(response => {
+          this.buyingLimit = response[0].buying_limit + 'Kg';
+          this.sellingLimit = response[0].selling_limit + 'Kg';
+        }, () => {
+
+        }
+      );
+  }
 }
