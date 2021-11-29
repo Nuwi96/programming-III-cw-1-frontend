2e448ef596d64d0a8523b25d1d84c0673217bbc6
Nuwi96 <92671065+Nuwi96@users.noreply.github.com>
2021-11-29 23:16:45 +0530

    update dashboard tiles

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -10,6 +10,8 @@ import {DatePipe} from '@angular/common';
 export class DashboardComponent implements OnInit {
   buyingLimit: any;
   sellingLimit: any;
+  buyingPrice: any;
+  sellingPrice: any;
 
   constructor(private daily_limitService: Daily_limitService, private datePipe: DatePipe) {
   }
@@ -22,8 +24,10 @@ export class DashboardComponent implements OnInit {
     var date = this.datePipe.transform(new Date(), 'yyyy-MM-dd');
     this.daily_limitService.getAllRecordsByDate(date)
       .subscribe(response => {
-          this.buyingLimit = response[0].buying_limit + 'Kg';
-          this.sellingLimit = response[0].selling_limit + 'Kg';
+          this.buyingLimit = response[0].buying_limit + ' Kg';
+          this.sellingLimit = response[0].selling_limit + ' Kg';
+          this.buyingPrice = 'Rs ' + response[0].buying_price ;
+          this.sellingPrice = 'Rs ' +response[0].selling_price;
         }, () => {
 
         }
